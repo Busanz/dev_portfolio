19e68c02dd5d3ade234cdf778b1bff070df15367
Busanz <buddhisanz@gmail.com>
2026-01-16 17:39:16 +0530

    Add images to the git projects

--- scripts/projects.js
+++ scripts/projects.js
@@ -9,29 +9,36 @@ const fetchGitHubProjects = async () => {
     if (!response.ok) {
       throw new Error('Failed to fetch repositories');
     }
-
+    const hiddenRepo = ['fg_assigment_03'];
     const repos = await response.json();
-    const visibleRepos = repos.filter((repo) => repo.has_pages === true);
+    const portfolioRepos = repos.filter(
+      (repo) => repo.has_pages === true && !hiddenRepo.includes(repo.name)
+    );
 
-    visibleRepos.forEach((repo) => {
+    portfolioRepos.forEach((repo) => {
       const publishUrl = `https://${GITHUB_USERNAME}.github.io/${repo.name}/`;
 
       const projectImage = {
         dev_portfolio: '../img/portfolio/dev_portfolio.png',
         'web-game-treasure-box': '../img/portfolio/web-game-treasure-box.png',
+        'fg-02-js-text-game': '../img/portfolio/fg-02-js-text-game.png',
+        versioncontrol_group_09: '../img/portfolio/versioncontrol_group_09.png',
       };
       const projectTitle = {
         dev_portfolio: 'Portfolio Website',
         'web-game-treasure-box': 'Treasure Box Game',
+        'fg-02-js-text-game': 'Text Base Web Game',
+        versioncontrol_group_09: 'Version Control Blog',
       };
-      const imageSrc = projectImage[repo.name];
+      const imageSrc =
+        projectImage[repo.name] || '../img/portfolio/project-placeholder.png';
       const titleOfProject = projectTitle[repo.name];
 
       const article = document.createElement('article');
       article.classList.add('wrapper__article');
 
       article.innerHTML = `
-        <div class="article_image">
+        <div class="article_image"> 
           <img
             src="${imageSrc}"
             alt="${repo.name}"
@@ -40,19 +47,18 @@ const fetchGitHubProjects = async () => {
         </div>
 
         <h3 class="article_title">${titleOfProject}</h3>
-
-        <p class="article_description">
+        <p class="hero__description">
           ${repo.description || 'No description provided.'}
         </p>
 
         <div class="article_icons">
           <a href="${
             repo.html_url
-          }" target="_blank" aria-label="GitHub Repository">
+          }" target="_blank" aria-label="GitHub Repository Link">
             <img src="./img/github.svg" alt="Git hub icon" width="40" height="40" />
           </a>
 
-          <a href="${publishUrl}" target="_blank" aria-label="Live Demo">
+          <a href="${publishUrl}" target="_blank" aria-label="Live Project">
             <img
               src="./img/globe.svg"
               alt="Live site icon"
